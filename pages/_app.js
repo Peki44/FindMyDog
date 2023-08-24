69983f191c1116261da4565c47ea1050ee25a0f5
Matej Pernar <pernarmatej@gmail.comtouch>
2023-08-24 16:27:10 +0200

    New4

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,3 @@
-import RootLayout from "@/app/layout.tsx"
- 
 export default function MyApp({ Component, pageProps }) {
-  return (
-      <RootLayout suppressHydrationWarning={true}>
-        <Component {...pageProps} />
-      </RootLayout>
-  )
+  return <Component {...pageProps} />
 }
