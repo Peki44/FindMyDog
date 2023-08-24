c09b5ec02d50849af20c47da7af60ce73c763052
Matej Pernar <pernarmatej@gmail.comtouch>
2023-08-24 15:57:50 +0200

    New3

--- src/app/components/molecules/UserForm/UserForm.tsx
+++ src/app/components/molecules/UserForm/UserForm.tsx
@@ -1,33 +1,85 @@
 import Image from "next/image";
 import Heading from "../../atoms/Heading";
 import Label from "../../atoms/Label";
-import styles from './UserForm.module.css';
+import styles from "./UserForm.module.css";
 import InfoLabel from "../../atoms/InfoLabel";
-
+ 
 export interface UserFormProps {
-    data?: any;
-}   
-
-const UserForm=(
-    {data}:UserFormProps
-)=>{
-    return <div className={styles.user_form}>
-        <Heading text={data.dogsName} size="_36" strong="bold"></Heading>
-        <div className={styles.image}>
-            <Image src={data.profileImageUri} alt="dog" width={160} height={160} id="image" className={styles.photo}></Image>
-        </div>
-        <div className={styles.forms}>
-            <Label text='Breed' strong="bold" color="white" size="_16"></Label>
-            <InfoLabel text={data.breed} id="breed" src="/paw.svg"></InfoLabel>
-            <Label text='Owner' strong="bold" color="white" size="_16"></Label>
-            <InfoLabel text={data.owner} id="owner" src="/boy.svg"></InfoLabel>
-            <Label text='Phone' strong="bold" color="white" size="_16"></Label>
-            <InfoLabel text={data.phone} id="phone" src="/phoneCall.svg"></InfoLabel>
-            <Label text='Email' strong="bold" color="white" size="_16"></Label>
-            <InfoLabel text={data.email} id="email" src="/mail.svg"></InfoLabel>
-            <Label text='Address' strong="bold" color="white" size="_16"></Label>
-            <InfoLabel text={data.address} id="address" src="/dogHouse.svg"></InfoLabel>
-        </div>
-    </div>
+  data?: any;
 }
+ 
+const UserForm = ({ data }: UserFormProps) => {
+  return (
+    <div className={styles.user_form}>
+      {data && (
+        <>
+          {data.dogsName && (
+            <Heading text={data.dogsName} size="_36" strong="bold"></Heading>
+          )}
+          {data.profileImageUri && (
+            <div className={styles.image}>
+              <Image
+                src={data.profileImageUri}
+                alt="dog"
+                width={100}
+                height={120}
+                id="image"
+              ></Image>
+            </div>
+          )}
+          <div className={styles.forms}>
+            {data.breed && (
+              <Label
+                text="Breed"
+                strong="bold"
+                color="white"
+                size="_16"
+              ></Label>
+            )}
+            {data.breed && <InfoLabel text={data.breed} id="breed" src="/paw.svg"></InfoLabel>}
+            {/* <Information_input type="text" placeholder="" id="breed"></Information_input> */}
+            {data.owner && (
+              <Label
+                text="Owner"
+                strong="bold"
+                color="white"
+                size="_16"
+              ></Label>
+            )}
+            {data.owner && <InfoLabel text={data.owner} id="owner" src="/boy.svg"></InfoLabel>}
+            {data.phone && (
+              <Label
+                text="Phone"
+                strong="bold"
+                color="white"
+                size="_16"
+              ></Label>
+            )}
+            {data.phone && <InfoLabel text={data.phone} id="phone" src="/phoneCall.svg"></InfoLabel>}
+            {data.email && (
+              <Label
+                text="Email"
+                strong="bold"
+                color="white"
+                size="_16"
+              ></Label>
+            )}
+            {data.email && <InfoLabel text={data.email} id="email" src="/mail.svg"></InfoLabel>}
+            {data.address && (
+              <Label
+                text="Address"
+                strong="bold"
+                color="white"
+                size="_16"
+              ></Label>
+            )}
+            {data.address && (
+              <InfoLabel text={data.address} id="address" src="/dogHouse.svg"></InfoLabel>
+            )}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 export default UserForm;
